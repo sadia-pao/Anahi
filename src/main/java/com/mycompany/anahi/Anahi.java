f37c8e6dcf7249a6595bb23eb5c8b3634e4012b0
sadia-pao <aandiyumbo@gmail.com>
2024-10-28 15:43:37 -0500

    primer comentrio

---
+++ src/main/java/com/mycompany/anahi/Anahi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.anahi;
+
+/**
+ *
+ * @author SO-LAB1-PC21
+ */
+public class Anahi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
